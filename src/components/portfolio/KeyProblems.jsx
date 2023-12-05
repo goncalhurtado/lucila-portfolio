1e1fe2c7cde85345704511f2b8da27c61f0bcce5
goncalhurtado <gonzalomilano@gmail.com>
2023-12-05 17:17:46 -0300

    working responsive to presto component

--- src/components/portfolio/KeyProblems.jsx
+++ src/components/portfolio/KeyProblems.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 const KeyProblems = ({ keyProblems }) => {
-  console.log(keyProblems);
   return (
     <>
       <div className="row">
         {keyProblems.map((data, index) => {
           return (
-            <div className="col text-center" key={index}>
+            <div className="col text-center keyproblems" key={index}>
               <h1>{data.number}</h1>
-              <h5>{data.title}</h5>
+              <h4>{data.title}</h4>
               <p>{data.description}</p>
             </div>
           );
