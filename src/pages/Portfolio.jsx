29712554c758643381f356ad942a21a3855317bd
goncalhurtado <gonzalomilano@gmail.com>
2023-12-05 14:13:48 -0300

    added styles to component component and fixed z-index on mobile navbar

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,17 +7,31 @@ const Portfolio = () => {
 
   return (
     <div className="containerBase">
-      <div
-        className="btn btn-primary"
-        onClick={() => navigate("/portfolio/presto")}
-      >
-        Presto
-      </div>
-      <div
-        className="btn btn-primary"
-        onClick={() => navigate("/portfolio/simile")}
-      >
-        Simile
+      <div className="portfolio">
+        <div
+          className="portfolio__project"
+          onClick={() => navigate("/portfolio/presto")}
+        >
+          <img
+            src="https://res.cloudinary.com/dhvgi2cmq/image/upload/v1701791196/tiliportoflio/Project-Presto_ycwjeu.png"
+            alt="presto"
+          />
+          <div className="portfolio__project__span">
+            <h2>Presto</h2>
+          </div>
+        </div>
+        <div
+          className="portfolio__project"
+          onClick={() => navigate("/portfolio/simile")}
+        >
+          <img
+            src="https://res.cloudinary.com/dhvgi2cmq/image/upload/v1701791197/tiliportoflio/Project-Simile_ppulya.png"
+            alt="simile"
+          />
+          <div className="portfolio__project__span">
+            <h2>Simile</h2>
+          </div>
+        </div>
       </div>
       <Contact />
     </div>
